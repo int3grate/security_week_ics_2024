a800c6b56d419ca1f3f9d2a9d8d2026bc15ef6d0
int3grate <namiles@gmail.com>
2024-10-15 12:35:19 -0400

    Update padding_oracle_attack.py

--- padding_oracle/padding_oracle_attack.py
+++ padding_oracle/padding_oracle_attack.py
@@ -1,15 +1,34 @@
+import binascii
+import base64
+import socket
+
 # Below is the data we'd like to decrypt using a remote padding oracle.  You will need to implement the code yourself to solve it.
 # I will upload the solution a week after the event.
 
 b64_data = "rDxVtvcl/fA6x33iBY8Bwo/1l7wEUT8SqRhi5WZFDVEz4KmsUkBXLXHH/CWCD5qg/LA1EIhVImmxBQDeMgZSo1w8IvVyUcq+SUh6jAerndo="
 b64_iv = "R40qxRE9vHKz18Oz1jaRjA=="
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_addr = 'boxofhorror.org'
+port = 4321
+sock.connect((ip_addr, port))
+
+res = sock.recv(1024)
 def call_decrypt(blocks):
     data = b''
     for i in range(1, len(blocks)):
         data += blocks[i]
     iv = blocks[0]
-    # implement call to server here
+
+    to_send = base64.b64encode(data) + b"," + base64.b64encode(iv) + b"\r\n"
+    sock.send(to_send)
+    data = sock.recv(2048)
+    data = data.strip()
+    data = data.decode('utf-8')
+    fields = data.split('\r')
+    
+    return fields[0]
   
 data = base64.b64decode(b64_data)
 data_len = len(data)
